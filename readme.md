0dcdb03ffe22892cafacb0ed022407eb19f8d501
mjankowski <mjankowski@7bulls.com>
2021-11-30 14:26:43 +0100

    Init

---
+++ readme.md
@@ -0,0 +1,6 @@
+# Goal
+
+Write application that will fetch all POSTS from JSONPlaceholder API.
+Each post should be stored in separate file <postId>.json
+
+https://jsonplaceholder.typicode.com/
---
+++ src/main/java/com/jankowskimichal/JsonPlaceholder/JsonPlaceholderApplication.java
@@ -0,0 +1,13 @@
+package com.jankowskimichal.JsonPlaceholder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonPlaceholderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JsonPlaceholderApplication.class, args);
+	}
+
+}
